df00c250e98517154baaab43485f5e9abb80dbcb
Miheret0506 <tmiheret29@gmail.com>
2022-10-22 09:04:04 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always success when  0 (Success)
+*/
+int main(void)
+{
+char low;
+for (low = 'a'; low <= 'z'; low++)
+putchar(low);
+for (low = 'A'; low <= 'Z'; low++)
+putchar(low);
+putchar('\n');
+return (0);
+}
